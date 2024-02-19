4d3a6eb21686151e87d042ecb0c701a83bcea3b3
Jason Cox <me@jasoncarloscox.com>
2024-02-19 09:54:06 -0500

    Fix pre-existing TS compile errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Map from '../components/Map';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className={styles['main']}>
       <div style={{ height: '800px' }}>
         <Map />
       </div>
