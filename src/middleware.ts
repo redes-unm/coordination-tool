17ce920dab2b3c22cf86f65d7d8fcae3efc574ff
Jason Cox <me@jasoncarloscox.com>
2024-06-05 17:58:13 -0400

    testing

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,8 +8,7 @@ export async function middleware(request: NextRequest) {
   if (!loggedIn && pathname.split('/')[1] !== 'auth') {
     const url = new URL(request.url);
     const afterLogin = encodeURIComponent(`${url.pathname}${url.search}`);
-    throw Error(`redirecting ${url} ${afterLogin} ${new URL(`/auth/login?redirect=${afterLogin}`, request.url)}`);
-    return NextResponse.redirect(new URL(`/auth/login?redirect=${afterLogin}`, request.url));
+    return NextResponse.redirect(`/auth/login?redirect=${afterLogin}`);
   }
 
   return response;
