5db82bc5ec46ba5381a4d57454f7923c409536a6
Beatriz Palacios <bepa@gatech.edu>
2024-06-06 23:59:30 -0400

    [WIP] Test tracking data on mount
    For receiving tracking data on the Edit Community Page.

--- src/app/communities/[communityId]/edit/page.tsx
+++ src/app/communities/[communityId]/edit/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import CommunityEditForm from '@/components/CommunityEditForm';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import CommunityContext from '@/contexts/CommunityContext';
 import { useRouter } from 'next/navigation';
 import { newDb } from '@/db/client';
-import { Collaborator, Community } from '@/types';
+import { Collaborator, Community, Trackables } from '@/types';
 import BreadcrumbContext from '@/contexts/BreadcrumbContext';
+import { useTracking } from 'react-tracking';
+
 import styles from './page.module.css';
 
 export default function EditCommunity() {
@@ -19,6 +21,26 @@ export default function EditCommunity() {
   } = useContext(CommunityContext);
 
   const { update: updateBreadcrumb } = useContext(BreadcrumbContext);
+
+  const {
+    getTrackingData,
+    // trackEvent
+  } = useTracking<Trackables>(
+    { page: `EditCommunity-${community.id}` },
+    {
+      dispatchOnMount: contextData => ({ // TODO test on mount
+        action: 'pageDataReady',
+        element: '', // TODO omit
+        page: `EditCommunity-${community.id}`,
+        timestamp: new Date().toISOString(),
+      }),
+    },
+  );
+
+  useEffect(() => {
+    const dataTest = getTrackingData();
+    console.log(`tracking data action? ${dataTest}`);
+  }, [getTrackingData]);
 
   const handleSave = async (comm: Community, collabs: Collaborator[]) => {
     await newDb().updateCommunity(comm, collabs);
