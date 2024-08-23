01eecede1f086af56c59c81b803a356b91993eea
Jason Cox <me@jasoncarloscox.com>
2024-08-23 13:53:09 -0400

    Handle missing user profiles gracefully
    When creating a new community, we fetch the user profiles of the current
    user and the admin user in order to display their names. Gracefully
    handle the case where one of these users doesn't have a profile by using
    a default name. (A user can only be missing a profile if the user was
    added to Supabase manually without a corresponding profile -- this issue
    shouldn't occur in prod.)
    
    Fixes: #73

--- src/app/communities/add/page.tsx
+++ src/app/communities/add/page.tsx
@@ -1,6 +1,7 @@
 import { newDb } from '@/db/server';
 import { getUser } from '@/lib/auth/server';
 import CommunityCreator from '@/components/CommunityCreator';
+import { Profile } from '@/types';
 import styles from './page.module.css';
 
 export default async function AddCommunity() {
@@ -8,9 +9,17 @@ export default async function AddCommunity() {
   const adminId = process.env['ADMIN_ID'];
   const user = await getUser();
 
+  async function getProfileOrDefault(userId: string, defaultName: string): Promise<Profile> {
+    try {
+      return await db.getProfile(userId);
+    } catch (e) {
+      return { userId, name: defaultName };
+    }
+  }
+
   const [userProfile, adminProfile] = await Promise.all([
-    db.getProfile(user.id),
-    adminId && user.id !== adminId ? db.getProfile(adminId) : null,
+    getProfileOrDefault(user.id, 'You'),
+    adminId && user.id !== adminId ? getProfileOrDefault(adminId, 'Admin') : null,
   ]);
 
   return (
